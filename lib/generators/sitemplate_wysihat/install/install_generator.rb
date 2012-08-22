47b9931f1fcbb4493ae77586ee77dc5386fd4d22
Yury Kotov <bairkan@gmail.com>
2012-08-22 18:40:56 +0400

    Возможность автоматического включения ассетов в приложение

--- lib/generators/sitemplate_wysihat/install/install_generator.rb
+++ lib/generators/sitemplate_wysihat/install/install_generator.rb
@@ -4,4 +4,18 @@ class SitemplateWysihat::InstallGenerator < Rails::Generators::Base
   def copy_engine
     copy_file 'jq-wysihat.js', 'public/jq-wysihat.js'
   end
+
+  def add_assets
+    filename = 'app/assets/javascripts/application.js'
+    File.open(filename) do |io|
+      str = '//= require sitemplate_wysihat'
+      append_file(filename, str) if io.grep(str).blank?
+    end
+
+    filename = 'app/assets/stylesheets/application.css'
+    File.open(filename) do |io|
+      str = " *= require sitemplate_wysihat\n"
+      inject_into_file(filename, str, before: '*/') if io.grep(str).blank?
+    end
+  end
 end
