67b0643ed6494057629fdd7ec81325ed89e4d3dd
Yury Kotov <bairkan@gmail.com>
2012-11-23 14:35:02 +0400

    Обработка ошибок при загрузке изображения

--- app/controllers/custom_images_controller.rb
+++ app/controllers/custom_images_controller.rb
@@ -10,7 +10,12 @@ class CustomImagesController < InheritedResources::Base
     @custom_image.width = params[:custom_image][:width].to_i
     @custom_image.height = params[:custom_image][:height].to_i
 
-    @custom_image.uploaded_file = params[:custom_image][:uploaded_file]
+    begin
+      @custom_image.uploaded_file = params[:custom_image][:uploaded_file]
+    rescue => e
+      logger.error e.message
+      logger.error e.backtrace.join("\n")
+    end
 
     unless @custom_image.save
       render json: @custom_image.errors.messages, status: 500
