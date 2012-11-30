f05ff5b71bcd578f81c1646f8f0cb4efd0ca89f6
Yury Kotov <bairkan@gmail.com>
2012-11-30 18:46:00 +0400

    Директория shared/select_image_dialog переименована для избежания конфликтов

--- app/helpers/sitemplate_wysihat_helper.rb
+++ app/helpers/sitemplate_wysihat_helper.rb
@@ -1,6 +1,6 @@
 module SitemplateWysihatHelper
   def render_image_selector
-    render partial: 'shared/select_image_dialog/markup', formats: [:html]
+    render partial: 'shared/sitemplate_select_image_dialog/markup', formats: [:html]
   end
 end
 
