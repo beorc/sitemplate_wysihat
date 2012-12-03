4bb85257c94988cec9d59ef52a78683321df763f
Yury Kotov <bairkan@gmail.com>
2012-12-03 15:27:39 +0400

    Явное добавление хелпера в приложение

--- lib/sitemplate_wysihat/engine.rb
+++ lib/sitemplate_wysihat/engine.rb
@@ -3,5 +3,10 @@ require 'carrierwave'
 
 module SitemplateWysihat
   class Engine < ::Rails::Engine
+    config.to_prepare do
+      ActiveSupport.on_load :action_controller do
+        ApplicationController.helper :sitemplate_wysihat
+      end
+    end
   end
 end
